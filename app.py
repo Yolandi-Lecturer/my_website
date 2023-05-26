7463af8dfbbb1630176d7a54177e4ca0a58024fe
Yolandi-Lecturer <yolandiev@hyperiondev.com>
2023-05-26 15:18:58 +0200

    added greet.html and layout.html

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 
 def index():
     return render_template('index.html') 
-    
+
+
+@app.route("/greet")
+def greet():
+    return render_template("greet.html", name=request.args.get("name", "world"))
